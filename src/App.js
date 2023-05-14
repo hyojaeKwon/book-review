4abbfd5a31088213b3b433e4201acdb0263961fd
hyojaeKwon <khj010909@gm.gist.ac.kr>
2023-05-14 11:01:49 +0000

    feat: react project created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MainPage from './components/mainPage';
+
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+			<h1> test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainPage.js
@@ -0,0 +1,10 @@
+// mainPage
+
+const MainPage = () => {
+	return 
+	<div>
+		<h1> This is Test Page</h1>
+	</div>
+}
+
+export default MainPage;
